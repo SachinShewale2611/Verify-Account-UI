0c0294ae8a7eeb93d50e5d9b68ca2ebdd1240995
Sachin Shewale <110060861+SachinShewale2611@users.noreply.github.com>
2022-12-17 17:59:14 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,11 +3,20 @@ codes[0].focus();
 
 codes.forEach((code, idx) => {
     code.addEventListener('keydown', (e) => {
-        if(e.key >= 0 && e.key <= 9) {
+        if(e.key >= 0 && e.key <= 9 ) {
             codes[idx].value = '';
+            if(idx==5) return;
             setTimeout(() => codes[idx +1].focus(),10)
-        } else if(e.key === 'Backspace') {
-            setTimeout(() => codes[idx].value = '' , codes[idx-1].focus(),10)
+        } else if(e.key === 'Backspace' && idx!=0) {
+            if(idx<5){
+                setTimeout(() =>  codes[idx].value = '', codes[idx-1].focus(),10)
+            }
+            else if(idx==5 && codes[idx].value == ""){
+                setTimeout(() =>  codes[idx].value = '', codes[idx-1].focus(),10)
+            }
+            else{
+                setTimeout(() =>  codes[idx].focus(),10)
+            }
         }
     })
 })
